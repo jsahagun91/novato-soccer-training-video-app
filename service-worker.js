5a4475ec9bf5b83fe0b6da1526fe1fc48340d7bd
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2025-08-23 16:57:13 -0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE='nst-rows-v1';const SHELL=['./','./index.html','./videos.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k!==CACHE?caches.delete(k):0))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  const dest=new URL(e.request.url).destination;
+  if(dest==='video') return; // stream from S3, don't cache
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
+  } else {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res;
+    })));
+  }
+});
